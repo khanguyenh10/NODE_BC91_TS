cbdf015a31dbd74d831c34bd3232b325aef9ecbb
khanguyenh10dev <khanguyenh10dev@gmail.com>
2026-05-26 15:41:55 +0700

    update

--- studentmanage/app/routers/student.router.js
+++ studentmanage/app/routers/student.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const studentRouter = express.Router();
-const { getStudentList, getStudentDetailById, createStudent, updateStudentById, deleteStudentById, deleteStudent } = require('../controllers/student.controllers');
+const { getStudentList, getStudentDetailById, createStudent, updateStudentById, deleteStudentById } = require('../controllers/student.controllers');
 const { logFeature } = require('../middleware/logger/log-feature');
 const { checkEmpty, checkGrade } = require('../middleware/validations/student.validation');
 
